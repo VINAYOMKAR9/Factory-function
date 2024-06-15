e0c28016ddc4844e8f8546745bcb9f8b71a4649b
VINAYOMKAR9 <vinayomkar9@gmail.com>
2024-06-15 16:22:12 +0530

    Fatory function

--- index.js
+++ index.js
@@ -18,11 +18,18 @@ function iPhone1(ASIN,color,display,camera){
 
 let data =iPhone1(44537,'blue','5.7','108')
 console.log(data);
+
+console.log(data.ASIN);
+console.log(data.color);
+console.log(data.display);
+console.log(data.camera);
+
+
 console.log(data.dial());
 console.log(data.sendMessage());
 console.log(data.cameraClick());
 
-//-------------------------------    problem 2      ----------------------------------------------------------------
+console.log('--------------------------------------------    problem 2      ------------------------------------------------');
 
 function iPhone2(ASIN,color,display,camera,Bluetooth){
     let obj = {};
@@ -50,6 +57,13 @@ function iPhone2(ASIN,color,display,camera,Bluetooth){
 
 let data2 =iPhone2(44537,'blue','5.7','108','5.3')
 console.log(data2);
+
+console.log(data2.ASIN);
+console.log(data2.color);
+console.log(data2.display);
+console.log(data2.camera);
+console.log(data2.Bluetooth);
+
 console.log(data2.dial());
 console.log(data2.sendMessage());
 console.log(data2.cameraClick());
